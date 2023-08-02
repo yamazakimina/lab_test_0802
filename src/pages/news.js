9fa24450f071e4edb5c051675f969522fc4bf84d
yamazakimina <130649486+yamazakimina@users.noreply.github.com>
2023-08-02 16:29:48 +0900

    練習

---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../styles/test.module.scss';
+
+const news = () => {
+  return (
+    <div class="text-5xl underline">news
+    <p className={style.test}>テスト</p>
+    </div>
+  )
+}
+
+export default news
